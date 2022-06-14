bb382b7d899ae1fb4650f5eb84304f0a4872cb20
ajohn893 <aaronjohnbivera@gmail.com>
2022-06-14 18:56:27 -0400

    fufufu

--- blog-service.js
+++ blog-service.js
@@ -2,9 +2,11 @@
 
 let posts = []
 let categories = []
- const fs = require("fs")
- exports.initialize = function() {
-   return new Promise(function(resolve, reject) {
+const { rejects } = require("assert")
+const fs = require("fs")
+const { resolve } = require("path")
+ exports.initialize = () => {
+   return new Promise((resolve, reject) => {
      fs.readFile("./data/posts.json", "utf8", function (err, data) {
        if (err) {
          reject("Unable to read the posts file!")
@@ -16,7 +18,7 @@ let categories = []
          console.log("Unparsable data, posts.json invalid or empty")
        }
      })
-     fs.readFile("./data/categories.json", "utf8", function(err, data) {
+     fs.readFile("./data/categories.json", "utf8", (err, data) => {
        if (err) {
          reject("Unable to read the categories file!")
          return
@@ -52,9 +54,29 @@ let categories = []
      resolve(published)
    })
  }
- 
- exports.getCategories = function() {
-   return new Promise(function(resolve, reject) {
+/*function getAllPosts() {
+	return promise
+		.then((result) => {
+			return result.posts;
+		})
+		.catch((error) => {
+			console.log(error);
+		});*/
+
+// exports.addPost (postData)
+function addPost(postData) {
+  return new Promise((resolve, rejects) => {
+    if (postData.published == 0) {
+      reject("Undefined")
+      return
+    }
+    resolve(addPost)
+  })
+}
+
+
+ exports.getCategories = () => {
+   return new Promise((resolve, reject) => {
      if (categories.length == 0) {
        reject("No results in Categories")
        return
@@ -63,3 +85,43 @@ let categories = []
    })
  }
  
+ /*module.exports.addPost = function (postData) {
+  var promise = new Promise(function (resolve, reject) {
+    if (postData) {
+      postData.id  = p.length + 1;
+      if (postData.published )
+        postData.published  = true;
+      else
+        postData.published  = false;
+      p.push(postData);
+      resolve(p);
+    }
+    else {
+      errMessage = "Something wrong function";
+      reject({ message: errMessage });
+    }
+  })
+  return promise;
+
+  module.exports.getPostsByMinDate = function (minDateStr) {
+    var post = [];
+  
+    var promise = new Promise((resolve, reject) => {
+  
+      for (var i = 0; i < p.length; i++) {
+        if(new Date(p[i].postDate) >= new Date(minDateStr)){
+      console.log("The postDate value is greater than minDateStr ")
+      post.push(p[i]);
+  }
+      }
+      if (post.length === 0) {
+        errMessage = "no results returned";
+        reject({ message: errMessage });
+      }
+      resolve(post);
+  
+    })
+    return promise;
+  };
+
+}*/
--- server.js
+++ server.js
@@ -1,21 +1,51 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: --Aaron John Bivera-- Student ID: --156486201-- Date: --06/06/2022--
+*
+*  Online (Heroku) URL: ___https://frozen-temple-45666.herokuapp.com____
+*
+*  GitHub Repository URL: ______https://github.com/ajohn893/web322-app.git________________________________________________
+*
+********************************************************************************/ 
 
 const express = require("express");
 const app = express();
 const path = require("path");
 const blog = require("./blog-service")
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+cloudinary.config({
+  cloud_name: 'dh0vyvebb',
+  api_key: '135839217257729',
+  api_secret: 'w-OcperQYlP5WQ3_j-KBLHjwpE4',
+  secure: true
+});
 
-app.get("/", function(req,res){
+const upload = multer(); 
+
+
+// remove comment after editing about.html file app.use(express.static("public"));
+
+app.get("/", (req,res) => {
   res.redirect("/about");
 });
 
-app.get("/about", function(req,res){
+app.get("/about", (req,res) => {
   res.sendFile(path.join(__dirname, "./views/about.html"));
 });
 
-app.get("/blog", function(req,res){
+app.get("/posts/add", (req,res) => {
+  res.sendFile(path.join(__dirname, "./views/addPost.html"));
+});
+
+
+app.get("/blog", (req,res) => {
   blog
     .getPublishedPosts().then((data) => {
       res.json({data})
@@ -24,7 +54,7 @@ app.get("/blog", function(req,res){
       res.json({message: err})
     })
 })
-app.get("/posts", function(req,res){
+app.get("/posts", (req,res) => {
   blog
     .getAllPosts().then((data) => {
       res.json({data})
@@ -33,7 +63,7 @@ app.get("/posts", function(req,res){
       res.json({message: err})
     })
   })
-app.get("/categories", function(req,res){
+app.get("/categories", (req,res) => {
     blog
       .getCategories().then((data) => {
         res.json({data})
@@ -43,12 +73,50 @@ app.get("/categories", function(req,res){
       })
 })
 
+app.post("/register-user", 
+upload.single("feautureimage"), (req, res) => {
+  res.send("register");
+  if(req.file){
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+
+    upload(req).then((uploaded)=>{
+        processPost(uploaded.url);
+    });
+}else{
+    processPost("");
+}
+ 
+function processPost(imageUrl){
+    req.body.featureImage = imageUrl;
+}
+});
+
 app.get("*", (req, res) => {
 	res.status(404).send(" 404 Page Not Found ⚠️");
 });
 
 blog
-  .initialize().then(function() {
+  .initialize().then(() => {
     app.listen(HTTP_PORT, () => {
       console.log(`Express http server listening on ${HTTP_PORT}`);
     });
