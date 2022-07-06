3d72cdf1c944ded7ee273657a7f516c1941c758e
ajohn893 <aaronjohnbivera@gmail.com>
2022-07-05 20:49:12 -0400

    jwfjvw

--- server.js
+++ server.js
@@ -65,22 +65,59 @@ app.use(function(req,res,next){
 });
 
 app.get("/", (req, res) => {
-  res.redirect("/about")
+  res.redirect("/blog")
 })
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/blog", (req, res) => {
-  blog
-    .getPublishedPosts()
-    .then((data) => {
-      res.json({ data })
-    })
-    .catch((err) => {
-      res.json({ message: err })
-    })
-})
+app.get('/blog', async (req, res) => {
+
+  // Declare an object to store properties for the view
+  let viewData = {};
+
+  try{
+
+      // declare empty array to hold "post" objects
+      let posts = [];
+
+      // if there's a "category" query, filter the returned posts by category
+      if(req.query.category){
+          // Obtain the published "posts" by category
+          posts = await blogData.getPublishedPostsByCategory(req.query.category);
+      }else{
+          // Obtain the published "posts"
+          posts = await blogData.getPublishedPosts();
+      }
+
+      // sort the published posts by postDate
+      posts.sort((a,b) => new Date(b.postDate) - new Date(a.postDate));
+
+      // get the latest post from the front of the list (element 0)
+      let post = posts[0]; 
+
+      // store the "posts" and "post" data in the viewData object (to be passed to the view)
+      viewData.posts = posts;
+      viewData.post = post;
+
+  }catch(err){
+      viewData.message = "no results";
+  }
+
+  try{
+      // Obtain the full list of "categories"
+      let categories = await blogData.getCategories();
+
+      // store the "categories" data in the viewData object (to be passed to the view)
+      viewData.categories = categories;
+  }catch(err){
+      viewData.categoriesMessage = "no results"
+  }
+
+  // render the "blog" view with all of the data (viewData)
+  res.render("blog", {data: viewData})
+
+});
 
 app.get("/categories", (req, res) => {
   blog
